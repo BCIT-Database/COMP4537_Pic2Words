a6ddf4a826a1d11c91ea7c703f608ac6639681c1
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2024-11-02 23:50:11 -0700

    py_server added

---
+++ py_server/app.py
@@ -0,0 +1,28 @@
+from flask import request, Flask
+from transformers import pipeline
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+pipe = pipeline("image-to-text",
+                model="Salesforce/blip-image-captioning-large")
+
+
+@app.route('/generate-caption', methods=['POST'])
+def generate_caption():
+    if 'image' not in request.files:
+        return "No image file provided", 400
+
+    image_file = request.files['image']
+    image = Image.open(io.BytesIO(image_file.read()))
+
+    try:
+        result = pipe(image)
+        return result
+    except Exception as e:
+        return str(e), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
