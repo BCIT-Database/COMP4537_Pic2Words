aee21c7c27eab6b4977489aa484596d9c4405eec
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2024-11-04 00:57:20 -0800

    home page

--- web/src/components/HomePage.jsx
+++ web/src/components/HomePage.jsx
@@ -4,15 +4,9 @@ import { useState, useEffect } from 'react';
 function HomePage() {
     const [message, setMessage] = useState('');
 
-    // useEffect(()=> {
-    //     fetch('http://localhost:8080/api/home')
-    //     .then((response)=> response.json())
-    //     .then((data)=> setMessage(data.message));
-    // }, []);
-
     const fetchData = async () => {
         try{
-            const response = await axios.get('http://localhost:5000/api/home');
+            const response = await axios.get('https://stingray-app-jmrty.ondigitalocean.app/api/home');
             setMessage(response.data.message)
         } catch (error) {
             console.error('error fetching data:', error);
