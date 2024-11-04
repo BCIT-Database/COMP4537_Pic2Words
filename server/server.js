d3fd94dbfdd7886556930ba430ba21732832a6a4
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2024-11-04 08:18:27 -0800

    deploy

--- server/server.js
+++ server/server.js
@@ -8,8 +8,6 @@ import path from "path";
 import homeRoutes from "./routes/homeRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-const port = process.env.PORT || 5000;
-
 // Connect to cloud storage
 connectCloudStorage();
 
@@ -44,22 +42,11 @@ app.use("/api", homeRoutes); // Set API path to '/api'
 // app.use("/api", uploadRoutes); // Commented out to avoid repetition
 app.use("/api/users", userRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use("/uploads", express.static("/var/data/uploads"));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  const __dirname = path.resolve();
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
-
+const port = process.env.PORT || 8080;
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 );
