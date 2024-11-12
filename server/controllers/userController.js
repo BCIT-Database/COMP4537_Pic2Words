e6794308757365b6d72f1dab8577a16fac013e25
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2024-11-12 10:15:48 -0800

    test

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -39,7 +39,7 @@ export const login = asyncHandler(async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
